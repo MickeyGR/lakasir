8841a3ca1eab8bdf7516d7d57c428e1160df2f79
Mickey GR <mickeyanthonygudiel@gmail.com>
2026-02-08 18:36:51 -0600

    fix(docs): restore manual security override to force global auth display

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -62,6 +62,9 @@ class AppServiceProvider extends ServiceProvider
             });
 
             Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
+                $openApi->secure(
+                    SecurityScheme::http('bearer')
+                );
 
                 // Add example response for POST /api/auth/login.
                 $loginExample = [
