7a0374166cb955a43cd37e876aaf02d11e24fd0e
Mickey GR <mickeyanthonygudiel@gmail.com>
2026-03-16 00:03:50 -0600

    Fix tenant image URLs and restored upload handling

--- app/MyBootstrapper.php
+++ app/MyBootstrapper.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\Request;
 use Stancl\Tenancy\Contracts\TenancyBootstrapper;
 use Stancl\Tenancy\Contracts\Tenant;
 
@@ -9,10 +10,25 @@ class MyBootstrapper implements TenancyBootstrapper
 {
     public function bootstrap(Tenant $tenant)
     {
-        //
+        if (! Request::instance()) {
+            return;
+        }
+
+        $baseUrl = Request::getSchemeAndHttpHost();
+
+        config([
+            'app.url' => $baseUrl,
+            'filesystems.disks.public.url' => "{$baseUrl}/storage",
+            'filesystems.disks.tmp.url' => "{$baseUrl}/tmp",
+        ]);
     }
 
     public function revert()
     {
+        config([
+            'app.url' => env('APP_URL', 'http://localhost'),
+            'filesystems.disks.public.url' => env('APP_URL', 'http://localhost').'/storage',
+            'filesystems.disks.tmp.url' => env('APP_URL', 'http://localhost').'/tmp',
+        ]);
     }
 }
